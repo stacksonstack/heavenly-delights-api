85785e89cb470884573a7b7ce6c41ea9a65ceb5b
stacksonstack <staceycarrillo44@gmail.com>
2021-04-21 17:43:15 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c407c002e6029aec5e000b91cf8634e9'),
+    },
+  },
+});
